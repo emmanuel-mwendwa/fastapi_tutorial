8c0f99013e5fd56366feb0fa07136550cce09fed
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-01-05 16:19:16 +0300

    Vote on posts system

--- app/schemas.py
+++ app/schemas.py
@@ -4,6 +4,8 @@ from datetime import datetime
 
 from typing import Optional
 
+from pydantic.types import conint
+
 
 class PostBase(BaseModel):
     title: str
@@ -51,3 +53,8 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
